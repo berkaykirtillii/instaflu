35d52aef69e4bb348c8c377d2f7b88f32859ff89
BerkayKirtilli <berkay.krtll.06@gmail.com>
2020-06-13 16:46:07 +0300

    instaflu.py modified

--- instaflu.py
+++ instaflu.py
@@ -1,14 +1,14 @@
 from flask import Flask,render_template,request,redirect,url_for
 from selenium import webdriver
 from time import sleep
-
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+import os
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+
 
 app = Flask(__name__)
 @app.route("/")
@@ -31,7 +31,7 @@ def learnUnfollowers():
 
             def __init__(self, username, pw):
                 
-                self.driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+                self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
                 self.driver.get("https://www.instagram.com/")
                 sleep(2)
                 self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
